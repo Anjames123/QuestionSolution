97b3575de89b8f41cc075ab5b8480a2a265bac76
root <root@DESKTOP-G0SDLVL.localdomain>
2025-01-09 02:27:59 -0800

    james

---
+++ Q_5.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/*
+ * Variables to store width and height of a rectangle in inches
+ */
+int width;
+int height;
+
+int area;
+int perimeter;
+
+int main()
+{
+	width = 5;
+	height = 7;
+
+	perimeter = 2*(height + width);
+
---
+++ Question_1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	// Print Name
+	printf("Name : Ajibade Aanuoluwa\n");
+
+	// print Date of birth
+	printf("DOB: July 14, 1975\n");
+
+	//Print Mobile number
+	printf("MObile : 99-999999999\n");
+
+	// Indicate successful execution
+	return(0);
+}
